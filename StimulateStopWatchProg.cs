5c51152484a1b6fb457bc62c360fd464f3c168a3
Subhash Barela <barelasubhash16@gmail.com>
2023-02-17 03:00:50 -0800

    Display the values in given  Time using StopWatch methode

---
+++ StimulateStopWatchProg.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FunctionalProgramingSeries
+{
+    internal class StimulateStopWatchProg
+    {
+        public static void StopWatch()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            
+            stopwatch.Start(); // Begin timing.
+
+           
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Welcome To BridgeLab Solution Training center");
+                Thread.Sleep(500); // we can also take wait methode
+            }
+
+            
+            stopwatch.Stop();   // Stop timing.
+
+            
+            Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
+        }
+    }
+}
